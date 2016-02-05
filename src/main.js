f6aec29e72b5c31d63d61d32acbaf06819f275bc
Jeremy Feinstein <jfeinstein@flatiron.com>
2016-02-05 13:19:03 -0500

    Switching to browserify and updating to fix the linter

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Rx from 'rx';
-import 'rx-dom';
-import {makeDOMDriver} from '@cycle/dom';
-import {parse} from 'virtual-dom';
-import VNode from 'virtual-dom/vnode/vnode';
-import VText from 'virtual-dom/vnode/vtext';
-import initializeConverter from 'html-to-vdom';
-import applyPatch from 'vdom-serialized-patch/patch';
-import toJson from 'vdom-as-json/toJson';
+'use strict';
+
+const _ = require('underscore');
+const Rx = require('rx-dom');
+const VNode = require('virtual-dom/vnode/vnode');
+const VText = require('virtual-dom/vnode/vtext');
+const initializeConverter = require('html-to-vdom');
+const applyPatch = require('vdom-serialized-patch/patch');
+const toJson = require('vdom-as-json/toJson');
 
 const events = `blur focus focusin focusout load resize scroll unload click
  dblclick mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave
@@ -14,52 +14,54 @@ const events = `blur focus focusin focusout load resize scroll unload click
  pointerdown pointerup pointermove pointerover pointerout pointerenter
  pointerleave touchstart touchend touchmove touchcancel`;
 
-workerRun('#app', '/dist/worker.js');
+const message = function (type, data) {
+  return { type, data };
+};
 
-function message(type, data) {
-  return {type, data};
-}
-
-function workerRun(container, script) {
+const workerRun = function (container, script) {
   let domContainer = typeof container === 'string' ?
     document.querySelector(container) :
     container;
   const worker$ = Rx.DOM.fromWorker(script);
 
   // Send all events on the container to the worker
-  function registerEvents(container) {
-    let eventObservables = events.split(' ').map((event) => {
-      return Rx.Observable.fromEvent(container, event);
+  const registerEvents = function (node) {
+    const eventObservables = events.split(' ').map((event) => {
+      return Rx.Observable.fromEvent(node, event);
     });
+
     Rx.Observable.merge(eventObservables).map((e) => {
-      let object = _.object(_.chain(e)
-        .allKeys()
-        .filter((key) => !_.isObject(e[key]))
-        .map((key) => [key, e[key]])
-        .value());
+      const object = _.object(_.chain(e).
+        allKeys().
+        filter((key) => !_.isObject(e[key])).
+        map((key) => [ key, e[key] ]).
+        value());
+
       return object;
     }).doOnNext((e) => {
       worker$.onNext(message('event', e));
     }).subscribe();
-  }
+  };
+
   registerEvents(domContainer);
 
   // Respond to messages from the worker
   const handlers = {
-    'patch': (data) => {
+    patch: (data) => {
       domContainer = applyPatch(domContainer, data);
       registerEvents(domContainer);
     }
   };
+
   worker$.subscribe((e) => {
     handlers[e.data.type](e.data.data);
-  }, console.log);
+  });
 
   // Send the initial contents of the container
-  const convertHTML = initializeConverter({
-    VNode: VNode,
-    VText: VText
-  });
+  const convertHTML = initializeConverter({ VNode, VText });
   const contents = toJson(convertHTML(domContainer.innerHTML));
+
   worker$.onNext(message('init', contents));
-}
+};
+
+workerRun('#app', '/dist/worker.js');
--- src/worker.js
+++ src/worker.js
@@ -1,42 +1,46 @@
-import Rx from 'rx';
-import {run} from '@cycle/core';
-import {h1} from '@cycle/dom';
-import VNode from 'virtual-dom/vnode/vnode';
-import diff from 'virtual-dom/diff';
-import fromJson from 'vdom-as-json/fromJson';
-import serializePatch from 'vdom-serialized-patch/serialize';
+'use strict';
 
-function message(type, data) {
-  return {type, data};
-}
+const Rx = require('rx');
+const { run } = require('@cycle/core');
+const { h1 } = require('@cycle/dom');
+const VNode = require('virtual-dom/vnode/vnode');
+const diff = require('virtual-dom/diff');
+const fromJson = require('vdom-as-json/fromJson');
+const serializePatch = require('vdom-serialized-patch/serialize');
 
-function transposeVTree(vtree) {
-  if (typeof vtree.subscribe === "function") {
+const message = function (type, data) {
+  return { type, data };
+};
+
+const transposeVTree = function (vtree) {
+  if (typeof vtree.subscribe === 'function') {
     return vtree.flatMapLatest(transposeVTree);
-  } else if (vtree.type === "VirtualText") {
+  } else if (vtree.type === 'VirtualText') {
     return Rx.Observable.just(vtree);
-  } else if (vtree.type === "VirtualNode" && Array.isArray(vtree.children) && vtree.children.length > 0) {
+  } else if (vtree.type === 'VirtualNode' && Array.isArray(vtree.children) && vtree.children.length > 0) {
     return Rx.Observable.combineLatest(vtree.children.map(transposeVTree), function () {
-      for (var _len = arguments.length, arr = Array(_len), _key = 0; _key < _len; _key++) {
-        arr[_key] = arguments[_key];
+      let arr, key, len;
+
+      for (len = arguments.length, arr = Array(len), key = 0; key < len; key++) {
+        arr[key] = arguments[key];
       }
       return new VNode(vtree.tagName, vtree.properties, arr, vtree.key, vtree.namespace);
     });
-  } else if (vtree.type === "VirtualNode" || vtree.type === "Widget" || vtree.type === "Thunk") {
+  } else if (vtree.type === 'VirtualNode' || vtree.type === 'Widget' || vtree.type === 'Thunk') {
     return Rx.Observable.just(vtree);
   } else {
-    throw new Error("Unhandled case in transposeVTree()");
+    throw new Error('Unhandled case in transposeVTree()');
   }
-}
+};
 
-function observerFromSelf() {
+const observerFromSelf = function () {
   // Create observer to handle sending messages
   return Rx.Observer.create((data) => {
     self.postMessage(data);
   });
-}
+};
 
-function observableFromSelf() {
+const observableFromSelf = function () {
   // Create observable to handle the messages
   return Rx.Observable.create((obs) => {
     self.addEventListener('message', (data) => {
@@ -47,52 +51,53 @@ function observableFromSelf() {
     });
     return () => {};
   });
-}
+};
 
-function makeWorkerDriver() {
-  let selfObserver$ = observerFromSelf();
-  let self$ = observableFromSelf().map((e) => e.data);
-  let init$ = self$.filter((data) => data.type === 'init')
-    .map((data) => fromJson(data.data));
-  init$.subscribe();
-  let events$ = self$.filter((data) => data.type === 'event')
-    .map((data) => data.data)
-    .do((e) => {
+const makeWorkerDriver = function () {
+  const selfObserver$ = observerFromSelf();
+  const self$ = observableFromSelf().map((e) => e.data);
+  const init$ = self$.filter((data) => data.type === 'init').
+    map((data) => fromJson(data.data));
+  const events$ = self$.filter((data) => data.type === 'event').
+    map((data) => data.data).
+    do(() => {
       // TODO map id to internal id and trigger event callback
-      console.log(e);
     });
+
+  init$.subscribe();
   events$.subscribe();
 
-  return function workerDriver(vtree$) {
+  return function workerDriver (vtree$) {
     // Parse the vtree and extract events
     // Pass the serialized patch
-    let output$ = init$.first()
-      .concat(vtree$)
-      .flatMapLatest(transposeVTree)
-      .pairwise()
-      .flatMap(([prevVTree, nextVTree]) => {
+    const output$ = init$.first().
+      concat(vtree$).
+      flatMapLatest(transposeVTree).
+      pairwise().
+      flatMap(([ prevVTree, nextVTree ]) => {
         // TODO add id's to nextVTree and store event callbacks
-        let patch = serializePatch(diff(prevVTree, nextVTree));
+        const patch = serializePatch(diff(prevVTree, nextVTree));
+
         selfObserver$.onNext(message('patch', patch));
         return Rx.Observable.just(nextVTree);
       });
+
     output$.subscribe();
     return {
       observable: output$,
       select: null
     };
   };
-}
+};
 
-function main({DOM}) {
-  console.log(DOM);
+const main = function ({ DOM }) {
   return {
-    DOM: Rx.Observable.interval(1000)
-      .map((i) => {
-        return h1([`${i} seconds elapsed`, h1('Foo')]);
+    DOM: Rx.Observable.interval(1000).
+      map((i) => {
+        return h1([ `${i} seconds elapsed`, h1('Foo') ]);
       })
   };
-}
+};
 
 const drivers = {
   DOM: makeWorkerDriver()
