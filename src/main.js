b64a6fe2784d156328411cee400e41329c6eca64
Jeremy Feinstein <jfeinstein@flatiron.com>
2016-02-10 10:29:12 -0500

    Events being passed back correctly

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ const initializeConverter = require('html-to-vdom');
 const applyPatch = require('vdom-serialized-patch/patch');
 const toJson = require('vdom-as-json/toJson');
 
-const events = `blur focus focusin focusout load resize scroll unload click
- dblclick mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave
- change select submit keydown keypress keyup error contextmenu
- pointerdown pointerup pointermove pointerover pointerout pointerenter
- pointerleave touchstart touchend touchmove touchcancel`;
+// const events = `blur focus focusin focusout load resize scroll unload click
+//  dblclick mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave
+//  change select submit keydown keypress keyup error contextmenu
+//  pointerdown pointerup pointermove pointerover pointerout pointerenter
+//  pointerleave touchstart touchend touchmove touchcancel`;
 
 const message = function (type, data) {
   return { type, data };
@@ -23,31 +23,43 @@ const workerRun = function (container, script) {
     document.querySelector(container) :
     container;
   const worker$ = Rx.DOM.fromWorker(script);
+  let eventStreamSubscription;
 
   // Send all events on the container to the worker
   const registerEvents = function (node) {
-    const eventObservables = events.split(' ').map((event) => {
-      return Rx.Observable.fromEvent(node, event);
-    });
-
-    Rx.Observable.merge(eventObservables).map((e) => {
+    const eventNodes = Array.prototype.slice.call(node.querySelectorAll('[cycle-events]'));
+    if (node.getAttribute('cycle-events')) {
+      eventNodes.push(node);
+    }
+    const eventObservables = _.flatten(_.map(eventNodes, (eventNode) => {
+      const events = eventNode.getAttribute('cycle-events');
+      return _.map(events.split(' '), (event) => Rx.Observable.fromEvent(eventNode, event.substring(2)));
+    }));
+    const eventStream$ = Rx.Observable.merge(eventObservables).map((e) => {
       const object = _.object(_.chain(e).
         allKeys().
         filter((key) => !_.isObject(e[key])).
         map((key) => [ key, e[key] ]).
         value());
 
-      return object;
-    }).doOnNext((e) => {
-      worker$.onNext(message('event', e));
-    }).subscribe();
+      return { id: e.target.getAttribute('cycle-id'), event: object };
+    });
+
+    eventStreamSubscription = eventStream$.subscribe((data) => {
+      worker$.onNext(message('event', data));
+    });
   };
 
-  registerEvents(domContainer);
+  const unregisterEvents = function () {
+    if (eventStreamSubscription) {
+      eventStreamSubscription.dispose();
+    }
+  };
 
   // Respond to messages from the worker
   const handlers = {
     patch: (data) => {
+      unregisterEvents();
       domContainer = applyPatch(domContainer, data);
       registerEvents(domContainer);
     }
@@ -55,6 +67,8 @@ const workerRun = function (container, script) {
 
   worker$.subscribe((e) => {
     handlers[e.data.type](e.data.data);
+  }, (e) => {
+    console.log(e);
   });
 
   // Send the initial contents of the container
--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('underscore');
 const Rx = require('rx');
 const { run } = require('@cycle/core');
 const { h1 } = require('@cycle/dom');
@@ -7,6 +8,16 @@ const VNode = require('virtual-dom/vnode/vnode');
 const diff = require('virtual-dom/diff');
 const fromJson = require('vdom-as-json/fromJson');
 const serializePatch = require('vdom-serialized-patch/serialize');
+const select = require("vtree-select");
+
+const guid = function () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+
+    return v.toString(16);
+  });
+};
 
 const message = function (type, data) {
   return { type, data };
@@ -36,7 +47,11 @@ const transposeVTree = function (vtree) {
 const observerFromSelf = function () {
   // Create observer to handle sending messages
   return Rx.Observer.create((data) => {
-    self.postMessage(data);
+    try {
+      self.postMessage(data);
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
 
@@ -53,15 +68,85 @@ const observableFromSelf = function () {
   });
 };
 
+const modifyEvents = function (events, vtree, id) {
+  const callbacks = {};
+  const eventsList = [];
+
+  vtree.properties.attributes = vtree.properties.attributes || {};
+  _.each(vtree.properties.attributes, function (callback, name) {
+    if (typeof name === 'string' && name.startsWith('on')) {
+      eventsList.push(name);
+      callbacks[name] = callback;
+      delete vtree.properties.attributes[name];
+    }
+  });
+  if (eventsList.length > 0) {
+    vtree.properties.attributes['cycle-id'] = id;
+    vtree.properties.attributes['cycle-events'] = eventsList.join(' ');
+    events.set(id, callbacks);
+  }
+  vtree.children.forEach(function (child, ix) {
+    if (child instanceof VNode) {
+      modifyEvents(events, child, `${id}${ix}`);
+    }
+  });
+};
+
+const makeEventsSelector = function (rootEl$) {
+  return (eventName) => {
+    if (typeof eventName !== `string`) {
+      throw new Error(`DOM driver's events() expects argument to be a ` +
+        `string representing the event type to listen for.`);
+    }
+    // TODO (jf) simulate bubbling!
+
+    return rootEl$.
+      first().
+      flatMapLatest(rootEl => {
+        return fromEvent(rootEl, eventName);
+      }).
+      share();
+  };
+};
+
+const makeElementSelector = function (rootEl$, namespace) {
+  return (selector) => {
+    if (typeof selector !== `string`) {
+      throw new Error(`DOM driver's select() expects the argument to be a ` +
+        `string as a CSS selector`);
+    }
+    selector = selector.trim();
+    if (selector !== `:root`) {
+      namespace = namespace.concat(selector);
+    }
+    const element$ = rootEl$.map((rootEl) => {
+      return select(selector, rootEl.join(` `));
+    });
+
+    return {
+      observable: element$,
+      select: makeElementSelector(rootEl$, namespace),
+      events: makeEventsSelector(rootEl$)
+    };
+  };
+};
+
 const makeWorkerDriver = function () {
   const selfObserver$ = observerFromSelf();
+  const events = new Map();
+  const id = guid();
   const self$ = observableFromSelf().map((e) => e.data);
   const init$ = self$.filter((data) => data.type === 'init').
     map((data) => fromJson(data.data));
   const events$ = self$.filter((data) => data.type === 'event').
     map((data) => data.data).
-    do(() => {
+    do((data) => {
       // TODO map id to internal id and trigger event callback
+      const callbackKey = `on${data.event.type}`;
+
+      if (events.has(data.id) && events.get(data.id)[callbackKey]) {
+        events.get(data.id)[callbackKey](data.event);
+      }
     });
 
   init$.subscribe();
@@ -70,22 +155,24 @@ const makeWorkerDriver = function () {
   return function workerDriver (vtree$) {
     // Parse the vtree and extract events
     // Pass the serialized patch
-    const output$ = init$.first().
+    const rootEl$ = init$.first().
       concat(vtree$).
       flatMapLatest(transposeVTree).
       pairwise().
       flatMap(([ prevVTree, nextVTree ]) => {
         // TODO add id's to nextVTree and store event callbacks
+        modifyEvents(events, nextVTree, `${id}-0`);
         const patch = serializePatch(diff(prevVTree, nextVTree));
 
         selfObserver$.onNext(message('patch', patch));
         return Rx.Observable.just(nextVTree);
       });
 
-    output$.subscribe();
+    rootEl$.subscribe();
     return {
-      observable: output$,
-      select: null
+      observable: rootEl$,
+      select: makeElementSelector(rootEl$, []),
+      events: makeEventsSelector(rootEl$)
     };
   };
 };
@@ -94,7 +181,17 @@ const main = function ({ DOM }) {
   return {
     DOM: Rx.Observable.interval(1000).
       map((i) => {
-        return h1([ `${i} seconds elapsed`, h1('Foo') ]);
+        return h1({
+          attributes: {
+            onclick: (e) => {
+              console.log(e);
+            },
+            onmouseover: (e) => {
+              console.log(e);
+            }
+          }
+        },
+        [ `${i} seconds elapsed`, h1('Foo') ]);
       })
   };
 };
